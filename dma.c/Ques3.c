dee0928ac0ea9a5856b310272d40deb73be8fdd4
ayush9085 <ayush1983pandey@gmail.com>
2025-11-17 18:51:18 +0530

    add Ques3.c for dynamic memory allocation and separation of even and odd elements

---
+++ dma.c/Ques3.c
@@ -0,0 +1,5 @@
+Q3. Write a C program that: 1. Dynamically allocates memory for n integers using malloc(). 2.
+Accepts elements from the user. 3. Separates all even and odd elements into two new dynamically
+allocated arrays. 4. Prints the even and odd arrays separately.
+Input Example: Enter number of elements: 6 Enter elements: 1 2 3 4 5 6
+Output Example: Even elements: 2 4 6 Odd elements: 1 3 5
