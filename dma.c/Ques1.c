9d6a4c515dbae0734cdb93cd362ae8b090456ee1
ayush9085 <ayush1983pandey@gmail.com>
2025-11-17 18:08:49 +0530

    add Ques1.c for dynamic array allocation and element insertion

---
+++ dma.c/Ques1.c
@@ -0,0 +1,8 @@
+/*Write a C program that: 1. Dynamically allocates an array of size n using malloc(). 2. Takes n
+integer inputs from the user. 3. Inserts a new element at a given position pos (1-based index). 4.
+Shifts the existing elements to the right. 5. Displays the updated array.
+Input Example: Enter number of elements: 5 Enter elements: 10 20 30 40 50 Enter element to
+insert: 25 Enter position to insert: 3
+Output Example: Array after insertion: 10 20 25 30 40 50
+*/
+
