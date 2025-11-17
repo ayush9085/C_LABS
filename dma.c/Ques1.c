df6b673539b8a5825b9afdd4013a159146622187
ayush9085 <ayush1983pandey@gmail.com>
2025-11-17 18:12:12 +0530

    add Ques1.c for dynamic array insertion functionality

--- dma.c/Ques1.c
+++ dma.c/Ques1.c
@@ -5,4 +5,50 @@ Input Example: Enter number of elements: 5 Enter elements: 10 20 30 40 50 Enter 
 insert: 25 Enter position to insert: 3
 Output Example: Array after insertion: 10 20 25 30 40 50
 */
+#include <stdio.h>
+#include <stdlib.h>
 
+void inputArray(int *arr, int n);
+void printArray(int *arr, int n);
+int* insertAtPosition(int *arr, int *n, int element, int pos);
+
+void inputArray(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
+void printArray(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int* insertAtPosition(int *arr, int *n, int element, int pos) {
+    arr = (int *)realloc(arr, (*n + 1) * sizeof(int));
+    for (int i = *n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[pos - 1] = element;
+    (*n)++;
+    return arr;
+}
+
+int main() {
+    int n, element, pos;
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+    inputArray(arr, n);
+
+    scanf("%d", &element);
+    scanf("%d", &pos);
+
+    arr = insertAtPosition(arr, &n, element, pos);
+
+    printArray(arr, n);
+
+    free(arr);
+    return 0;
+}
