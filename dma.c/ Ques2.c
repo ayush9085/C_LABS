42b444d08f52350145ea91db6098ac2b1edf7c15
ayush9085 <ayush1983pandey@gmail.com>
2025-11-17 18:20:38 +0530

    add Ques2.c for dynamic memory allocation and frequency counting functionality

---
+++ dma.c/ Ques2.c
@@ -0,0 +1,5 @@
+// Q2. Write a C program that: 1. Dynamically allocates memory for n integers using malloc(). 2.
+// Takes array input from the user. 3. Counts and displays the frequency of each unique element in
+// the array.
+// Input Example: Enter number of elements: 6 Enter elements: 2 3 2 5 3 3
+// Output Example: Element 2 appears 2 times Element 3 appears 3 times Element 5 appears 1 time
