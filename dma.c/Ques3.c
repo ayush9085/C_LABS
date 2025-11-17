deb36de5de7e2b565b866c9a5f11088b0f9b0208
ayush9085 <ayush1983pandey@gmail.com>
2025-11-17 18:52:48 +0530

    add Ques3.c for dynamic memory allocation and separation of even and odd elements

--- dma.c/Ques3.c
+++ dma.c/Ques3.c
@@ -3,3 +3,72 @@
 // allocated arrays. 4. Prints the even and odd arrays separately.
 // Input Example: Enter number of elements: 6 Enter elements: 1 2 3 4 5 6
 // Output Example: Even elements: 2 4 6 Odd elements: 1 3 5
+#include <stdio.h>
+#include <stdlib.h>
+
+void inputArray(int *arr, int n);
+void printArray(int *arr, int n);
+void separateEvenOdd(int *arr, int n, int **evenArr, int *eCount, int **oddArr, int *oCount);
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    printf("Enter elements: ");
+    inputArray(arr, n);
+
+    int *evenArr, *oddArr;
+    int eCount = 0, oCount = 0;
+
+    separateEvenOdd(arr, n, &evenArr, &eCount, &oddArr, &oCount);
+
+    printf("Even elements: ");
+    printArray(evenArr, eCount);
+
+    printf("Odd elements: ");
+    printArray(oddArr, oCount);
+
+    free(arr);
+    free(evenArr);
+    free(oddArr);
+
+    return 0;
+}
+
+void inputArray(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
+void printArray(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void separateEvenOdd(int *arr, int n, int **evenArr, int *eCount, int **oddArr, int *oCount) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0)
+            (*eCount)++;
+        else
+            (*oCount)++;
+    }
+
+    *evenArr = (int *)malloc((*eCount) * sizeof(int));
+    *oddArr = (int *)malloc((*oCount) * sizeof(int));
+
+    int ei = 0, oi = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0)
+            (*evenArr)[ei++] = arr[i];
+        else
+            (*oddArr)[oi++] = arr[i];
+    }
+}
